3487209260d1750596bd57a20590722d5d7db8c7
Stephan Hagemann <stephan@pivotallabs.com>
2013-06-24 16:23:57 -0600

    Extract goroutine from json watcher
    Signed-off-by: Alex Jackson <ajackson@pivotallabs.com>

--- sources/dea_logging_agent/src/dea_logging_agent/dea_logging_agent.go
+++ sources/dea_logging_agent/src/dea_logging_agent/dea_logging_agent.go
@@ -30,44 +30,46 @@ func WatchInstancesJsonFileForChanges(config *Config) (chan InstanceEvent) {
 	steno.Init(loggingConfig)
 	logger := steno.NewLogger("dea_logging_agent")
 
-	go func() {
-		for {
-			json, err := ioutil.ReadFile(config.instancesJsonFilePath)
-			if (err != nil) {
-				logger.Warnf("Reading failed. %v\n", err)
-				close(instancesChan)
-				return
-			}
+	go pollInstancesJson(config.instancesJsonFilePath, instancesChan, knownInstances, logger)
 
-			time.Sleep(1*time.Millisecond)
-			currentInstances, err := ReadInstances(json)
-			if (err != nil) {
-				logger.Warnf("Failed parsing json %v: %v Trying again...\n", err, string(json))
-				runtime.Gosched()
+	return instancesChan
+}
+
+func pollInstancesJson(instancesJsonFilePath string, instancesChan chan InstanceEvent, knownInstances map[string]Instance, logger *steno.Logger) {
+	for {
+		json, err := ioutil.ReadFile(instancesJsonFilePath)
+		if (err != nil) {
+			logger.Warnf("Reading failed. %v\n", err)
+			close(instancesChan)
+			return
+		}
+
+		time.Sleep(1*time.Millisecond)
+		currentInstances, err := ReadInstances(json)
+		if (err != nil) {
+			logger.Warnf("Failed parsing json %v: %v Trying again...\n", err, string(json))
+			runtime.Gosched()
+			continue
+		}
+
+		for _, instance := range knownInstances {
+			_, present := currentInstances[instance.Identifier()]
+			if present {
 				continue
 			}
 
-			for _, instance := range knownInstances {
-				_, present := currentInstances[instance.Identifier()]
-				if present {
-					continue
-				}
-
-				delete(knownInstances, instance.Identifier())
-				instancesChan <- InstanceEvent{instance, false}
-			}
-
-			for _, instance := range currentInstances {
-				_, present := knownInstances[instance.Identifier()]
-				if present {
-					continue
-				}
-
-				knownInstances[instance.Identifier()] = instance
-				instancesChan <- InstanceEvent{instance, true}
-			}
+			delete(knownInstances, instance.Identifier())
+			instancesChan <- InstanceEvent{instance, false}
 		}
-	}()
 
-	return instancesChan
+		for _, instance := range currentInstances {
+			_, present := knownInstances[instance.Identifier()]
+			if present {
+				continue
+			}
+
+			knownInstances[instance.Identifier()] = instance
+			instancesChan <- InstanceEvent{instance, true}
+		}
+	}
 }
