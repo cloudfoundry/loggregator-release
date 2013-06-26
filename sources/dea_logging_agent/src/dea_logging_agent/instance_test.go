4e3e3de3fdb9e5bb8911525e6fe2b7dd3348b033
Stephan Hagemann <stephan@pivotallabs.com>
2013-06-26 11:13:11 -0600

    Fixing CI

--- sources/dea_logging_agent/src/dea_logging_agent/instance_test.go
+++ sources/dea_logging_agent/src/dea_logging_agent/instance_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"github.com/stretchr/testify/assert"
 	"path/filepath"
+	"io/ioutil"
 	"os"
 	"bytes"
 	"net"
@@ -27,24 +28,30 @@ func (m *MockSinkServer) Send(data []byte) {
 }
 
 func TestThatWeListenToTheUnixSockets(t *testing.T) {
+	tmpdir, err := ioutil.TempDir("", "testing")
+	assert.NoError(t, err)
+	defer os.RemoveAll(tmpdir)
+
 	instance := &Instance{
 		ApplicationId: "1234",
 		WardenJobId: "56",
-		WardenContainerPath: "/tmp" }
+		WardenContainerPath: tmpdir }
+	os.MkdirAll(instance.Identifier(), 0777)
+
 	socketPath := filepath.Join(instance.Identifier(), "stdout.sock")
 	os.Remove(socketPath)
 	expectedOutput := bytes.NewBufferString("Some Output\n").Bytes()
 
 	stdoutListener, err := net.Listen("unix", socketPath)
 	assert.NoError(t, err)
+
 	go func() {
 		connection, err := stdoutListener.Accept()
 		defer connection.Close()
-		assert.NoError(t, err)
+		assert.Nil(t, err)
 
 		_, err = connection.Write(expectedOutput)
 		assert.NoError(t, err)
-
 	}()
 
 	mockSinkServer := new(MockSinkServer)
