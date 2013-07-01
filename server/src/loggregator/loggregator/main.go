da42afc5c5c0e1607c3b04548ec704fb7f893490
Alex Jackson <ajackson@pivotallabs.com>
2013-07-01 15:09:21 -0600

    main impl for loggregator
    Signed-off-by: Mike Gehard <mgehard@pivotallabs.com>

--- server/src/loggregator/loggregator/main.go
+++ server/src/loggregator/loggregator/main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/cloudfoundry/gosteno"
+	"strings"
+	"os"
+	"loggregator"
 )
 
 var version = flag.Bool("version", false, "Version info")
+var host = flag.String("host", ":3456", "server ip:port to listen")
 
-const versionNumber = `0.0.1.`
-const gitSha = ``
+const versionNumber = `0.0.1.TRAVIS_BUILD_NUMBER`
+const gitSha = `TRAVIS_COMMIT`
 
 func main() {
 	flag.Parse()
@@ -17,4 +22,30 @@ func main() {
 		fmt.Printf("\n\nversion: %s\ngitSha: %s\n\n", versionNumber, gitSha)
 		return
 	}
+
+	level := gosteno.LOG_INFO
+
+	if *logLevel {
+		level = gosteno.LOG_DEBUG
+	}
+
+	loggingConfig := &gosteno.Config{
+		Sinks:     make([]gosteno.Sink, 1),
+		Level:     level,
+		Codec:     gosteno.NewJsonCodec(),
+		EnableLOC: true}
+
+	if strings.TrimSpace(*logFilePath) == "" {
+		loggingConfig.Sinks[0] = gosteno.NewIOSink(os.Stdout)
+	} else {
+		loggingConfig.Sinks[0] = gosteno.NewFileSink(*logFilePath)
+	}
+	gosteno.Init(loggingConfig)
+	logger := gosteno.NewLogger("loggregator")
+
+	listener := loggregator.NewAgentListener(host, logger)
+	incomingData := listener.Start()
+
+	cfSink := loggregator.NewCfSink(incomingData, logger)
+	cfSink.Start()
 }
