7628cf98f41ba29e281ff6e82c29e02c6fdc3ca1
Stephan Hagemann <stephan@pivotallabs.com>
2013-07-03 11:01:46 -0600

    Embed untested functions into instance.startListening()

--- sources/deaagent/src/deaagent/instance.go
+++ sources/deaagent/src/deaagent/instance.go
@@ -22,44 +22,44 @@ func (instance *instance) identifier() string {
 	return filepath.Join(instance.wardenContainerPath, "jobs", strconv.FormatUint(instance.wardenJobId, 10))
 }
 
-func (instance *instance) startListening(loggregatorClient loggregatorclient.LoggregatorClient) {
-	stdoutSocket := filepath.Join(instance.identifier(), "stdout.sock")
-	go instance.listen(stdoutSocket, loggregatorClient, instance.logPrefix("STDOUT"))
-
-	stderrSocket := filepath.Join(instance.identifier(), "stderr.sock")
-	go instance.listen(stderrSocket, loggregatorClient, instance.logPrefix("STDERR"))
-}
-
-func (instance *instance) logPrefix(socketName string) (string) {
-	return strings.Join([]string{instance.applicationId, strconv.FormatUint(instance.index, 10), socketName}, " ")
-}
-
-func (instance *instance) listen(socket string, loggregatorClient loggregatorclient.LoggregatorClient, prefix string) {
-	connection, err := net.Dial("unix", socket)
-	if err != nil {
-		instance.logger.Fatalf("Error while dialing into socket %s, %s", socket, err)
-		return
+func (inst *instance) startListening(loggregatorClient loggregatorclient.LoggregatorClient) {
+	logPrefix := func(inst *instance, socketName string) (string) {
+		return strings.Join([]string{inst.applicationId, strconv.FormatUint(inst.index, 10), socketName}, " ")
 	}
-	defer func() {
-		connection.Close()
-		instance.logger.Infof("Stopped reading from socket %s", socket)
-	}()
-	prefixBytes := []byte(prefix + " ")
-	prefixLength := len(prefixBytes)
-	buffer := make([]byte, prefixLength + bufferSize)
 
-	//we're copying (and keeping) the message prefix in the buffer so every loggregatorClient.Send will have the prefix
-	copy(buffer[0:prefixLength], prefixBytes)
-
-	for {
-		readCount, err := connection.Read(buffer[prefixLength:])
-		if readCount == 0 && err != nil {
-			instance.logger.Infof("Error while reading from socket %s, %s", socket, err)
-			break
+	listen := func(logger *gosteno.Logger, socket string, loggregatorClient loggregatorclient.LoggregatorClient, prefix string) {
+		connection, err := net.Dial("unix", socket)
+		if err != nil {
+			logger.Fatalf("Error while dialing into socket %s, %s", socket, err)
+			return
 		}
-		instance.logger.Debugf("Read %d bytes from socket", readCount)
-		loggregatorClient.Send(buffer[:readCount + prefixLength])
-		instance.logger.Debugf("Sent %d bytes to loggregator", readCount)
-		runtime.Gosched()
+		defer func() {
+			connection.Close()
+			logger.Infof("Stopped reading from socket %s", socket)
+		}()
+		prefixBytes := []byte(prefix + " ")
+		prefixLength := len(prefixBytes)
+		buffer := make([]byte, prefixLength + bufferSize)
+
+		//we're copying (and keeping) the message prefix in the buffer so every loggregatorClient.Send will have the prefix
+		copy(buffer[0:prefixLength], prefixBytes)
+
+		for {
+			readCount, err := connection.Read(buffer[prefixLength:])
+			if readCount == 0 && err != nil {
+				logger.Infof("Error while reading from socket %s, %s", socket, err)
+				break
+			}
+			logger.Debugf("Read %d bytes from instance socket", readCount)
+			loggregatorClient.Send(buffer[:readCount + prefixLength])
+			logger.Debugf("Sent %d bytes to loggregator client", readCount)
+			runtime.Gosched()
+		}
 	}
+
+	stdoutSocket := filepath.Join(inst.identifier(), "stdout.sock")
+	go listen(inst.logger, stdoutSocket, loggregatorClient, logPrefix(inst, "STDOUT"))
+
+	stderrSocket := filepath.Join(inst.identifier(), "stderr.sock")
+	go listen(inst.logger, stderrSocket, loggregatorClient, logPrefix(inst, "STDERR"))
 }
