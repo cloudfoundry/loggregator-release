d053facf5e05cc07e5ff28e9e4092ba936a1e9b3
Stephan Hagemann <stephan@pivotallabs.com>
2013-08-01 10:28:31 -0600

    Added a test for the instrumentation of the agentListener
    Signed-off-by: Alex Jackson <ajackson@pivotallabs.com>

--- server/src/loggregator/agentlistener/agent_listener_test.go
+++ server/src/loggregator/agentlistener/agent_listener_test.go
@@ -3,13 +3,20 @@ package agentlistener
 import (
 	"github.com/cloudfoundry/gosteno"
 	"github.com/stretchr/testify/assert"
+	"instrumentor"
 	"net"
 	"testing"
 )
 
+var listener *agentListener
+var dataChannel chan []byte
+
+func init() {
+	listener = NewAgentListener("localhost:3456", gosteno.NewLogger("TestLogger"))
+	dataChannel = listener.Start()
+}
+
 func TestThatItListens(t *testing.T) {
-	listener := NewAgentListener("localhost:3456", gosteno.NewLogger("TestLogger"))
-	dataChannel := listener.Start()
 
 	expectedData := "Some Data"
 	otherData := "More stuff"
@@ -25,6 +32,20 @@ func TestThatItListens(t *testing.T) {
 	received := <-dataChannel
 	assert.Equal(t, expectedData, string(received))
 
+	expectedDumpedData := []instrumentor.PropVal{
+		instrumentor.PropVal{"CurrentBufferCount", "1"},
+		instrumentor.PropVal{"ReceivedMessageCount", "2"},
+		instrumentor.PropVal{"ReceivedByteCount", "19"},
+	}
+	assert.Equal(t, expectedDumpedData, listener.DumpData())
+
 	receivedAgain := <-dataChannel
 	assert.Equal(t, otherData, string(receivedAgain))
+
+	expectedDumpedData = []instrumentor.PropVal{
+		instrumentor.PropVal{"CurrentBufferCount", "0"},
+		instrumentor.PropVal{"ReceivedMessageCount", "2"},
+		instrumentor.PropVal{"ReceivedByteCount", "19"},
+	}
+	assert.Equal(t, expectedDumpedData, listener.DumpData())
 }
