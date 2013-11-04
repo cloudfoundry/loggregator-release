5a812cd4e01a9cc2aaa6e1b927e2db791dd1d87e
Stephan Hagemann <stephan@pivotallabs.com>
2013-11-04 13:22:52 -0700

    Test server that outputs the type and data of a extracted protobuffer
    [#59349950]
    
    Signed-off-by: Matthew McNew <mmcnew@pivotallabs.com>

---
+++ protobuf_tester/src/protobuftester/protobuftester/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"github.com/cloudfoundry/loggregatorlib/agentlistener"
+	"github.com/cloudfoundry/loggregatorlib/loggertesthelper"
+	"flag"
+	"code.google.com/p/gogoprotobuf/proto"
+	"github.com/cloudfoundry/loggregatorlib/logmessage"
+	"fmt"
+)
+
+var host = flag.String("host", "0.0.0.0:3456", "The host/port you want to listen on")
+var secret = flag.String("secret", "", "Our secret")
+
+func main() {
+	logger := loggertesthelper.Logger()
+
+	flag.Parse()
+
+	listener := agentlistener.NewAgentListener(*host, logger)
+	dataChannel := listener.Start()
+
+	for data := range dataChannel {
+		if logMessage, err := extractLogMessage(data); err == nil {
+			fmt.Printf("MESSAGE: %+v\n", logMessage)
+		} else if logEnvelope, err := extractLogEnvelope(data); err == nil {
+			validEnvelope := logEnvelope.VerifySignature(*secret)
+			fmt.Printf("ENVELOPE (valid: %t): %+v\n", validEnvelope, logEnvelope)
+		} else {
+			fmt.Println("ERROR parsing protobuffer")
+		}
+	}
+}
+
+func extractLogMessage(data []byte) (*logmessage.LogMessage, error) {
+	logMessage := new(logmessage.LogMessage)
+	err := proto.Unmarshal(data, logMessage)
+	if err != nil {
+		return logMessage, err
+	}
+	return logMessage, nil
+}
+
+func extractLogEnvelope(data []byte) (*logmessage.LogEnvelope, error) {
+	logEnvelope := new(logmessage.LogEnvelope)
+	err := proto.Unmarshal(data, logEnvelope)
+	if err != nil {
+		return logEnvelope, err
+	}
+	return logEnvelope, nil
+}
