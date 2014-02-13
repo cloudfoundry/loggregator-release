21817d53281350ba398a5154353aff007049e5b6
Alex Jackson <ajackson@pivotallabs.com>
2014-02-13 08:54:57 -0700

    use copy of localhost certs as encoded []byte constants rather than generating files
    Signed-off-by: Chris Wang <cwang@pivotallabs.com>

--- src/loggregator/sinks/syslogwriter/syslog_writer_test.go
+++ src/loggregator/sinks/syslogwriter/syslog_writer_test.go
@@ -1,22 +1,14 @@
 package syslogwriter_test
 
 import (
-	"crypto/rand"
-	"crypto/rsa"
 	"crypto/tls"
-	"crypto/x509"
-	"crypto/x509/pkix"
-	"encoding/pem"
 	"github.com/cloudfoundry/gosteno"
 	"github.com/cloudfoundry/loggregatorlib/loggertesthelper"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"loggregator/sinks/syslogwriter"
-	"math/big"
 	"net"
 	"net/url"
-	"os"
-	"strings"
 	"time"
 	"net/http/httptest"
 	"net/http"
@@ -205,8 +197,7 @@ func startSyslogServer(shutdownChan <-chan bool) (<-chan []byte, <-chan bool) {
 
 func startTLSSyslogServer(shutdownChan <-chan bool, logger *gosteno.Logger) <-chan bool {
 	doneChan := make(chan bool)
-	generateCert(logger)
-	cert, err := tls.LoadX509KeyPair("cert.pem", "key.pem")
+	cert, err := tls.X509KeyPair(localhostCert, localhostKey)
 	if err != nil {
 		panic(err)
 	}
@@ -241,90 +232,24 @@ func startTLSSyslogServer(shutdownChan <-chan bool, logger *gosteno.Logger) <-ch
 	return doneChan
 }
 
-func generateCert(logger *gosteno.Logger) {
-	// see: http://golang.org/src/pkg/crypto/tls/generate_cert.go
-	host := "localhost"
-	validFrom := "Jan 1 15:04:05 2011"
-	validFor := 10 * 365 * 24 * time.Hour
-	isCA := true
-	rsaBits := 1024
+var localhostCert = []byte(`-----BEGIN CERTIFICATE-----
+MIIBdzCCASOgAwIBAgIBADALBgkqhkiG9w0BAQUwEjEQMA4GA1UEChMHQWNtZSBD
+bzAeFw03MDAxMDEwMDAwMDBaFw00OTEyMzEyMzU5NTlaMBIxEDAOBgNVBAoTB0Fj
+bWUgQ28wWjALBgkqhkiG9w0BAQEDSwAwSAJBAN55NcYKZeInyTuhcCwFMhDHCmwa
+IUSdtXdcbItRB/yfXGBhiex00IaLXQnSU+QZPRZWYqeTEbFSgihqi1PUDy8CAwEA
+AaNoMGYwDgYDVR0PAQH/BAQDAgCkMBMGA1UdJQQMMAoGCCsGAQUFBwMBMA8GA1Ud
+EwEB/wQFMAMBAf8wLgYDVR0RBCcwJYILZXhhbXBsZS5jb22HBH8AAAGHEAAAAAAA
+AAAAAAAAAAAAAAEwCwYJKoZIhvcNAQEFA0EAAoQn/ytgqpiLcZu9XKbCJsJcvkgk
+Se6AbGXgSlq+ZCEVo0qIwSgeBqmsJxUu7NCSOwVJLYNEBO2DtIxoYVk+MA==
+-----END CERTIFICATE-----`)
 
-	if len(host) == 0 {
-		logger.Fatalf("Missing required --host parameter")
-	}
-
-	priv, err := rsa.GenerateKey(rand.Reader, rsaBits)
-	if err != nil {
-		logger.Fatalf("failed to generate private key: %s", err)
-		panic(err)
-	}
-
-	var notBefore time.Time
-	if len(validFrom) == 0 {
-		notBefore = time.Now()
-	} else {
-		notBefore, err = time.Parse("Jan 2 15:04:05 2006", validFrom)
-		if err != nil {
-			logger.Fatalf("Failed to parse creation date: %s\n", err)
-			panic(err)
-		}
-	}
-
-	notAfter := notBefore.Add(validFor)
-
-	// end of ASN.1 time
-	endOfTime := time.Date(2049, 12, 31, 23, 59, 59, 0, time.UTC)
-	if notAfter.After(endOfTime) {
-		notAfter = endOfTime
-	}
-
-	template := x509.Certificate{
-		SerialNumber: new(big.Int).SetInt64(0),
-		Subject: pkix.Name{
-			Organization: []string{"Loggregator TrafficController TEST"},
-		},
-		NotBefore:             notBefore,
-		NotAfter:              notAfter,
-		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
-		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
-		BasicConstraintsValid: true,
-	}
-
-	hosts := strings.Split(host, ",")
-	for _, h := range hosts {
-		if ip := net.ParseIP(h); ip != nil {
-			template.IPAddresses = append(template.IPAddresses, ip)
-		} else {
-			template.DNSNames = append(template.DNSNames, h)
-		}
-	}
-
-	if isCA {
-		template.IsCA = true
-		template.KeyUsage |= x509.KeyUsageCertSign
-	}
-
-	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, &priv.PublicKey, priv)
-	if err != nil {
-		logger.Fatalf("Failed to create certificate: %s", err)
-		panic(err)
-	}
-
-	certOut, err := os.Create("cert.pem")
-	if err != nil {
-		logger.Fatalf("failed to open cert.pem for writing: %s", err)
-		panic(err)
-	}
-	pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
-	certOut.Close()
-	logger.Info("written cert.pem\n")
-
-	keyOut, err := os.OpenFile("key.pem", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
-	if err != nil {
-		logger.Fatalf("failed to open key.pem for writing: %s", err)
-		panic(err)
-	}
-	pem.Encode(keyOut, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(priv)})
-	keyOut.Close()
-	logger.Info("written key.pem\n")
-}
+// localhostKey is the private key for localhostCert.
+var localhostKey = []byte(`-----BEGIN RSA PRIVATE KEY-----
+MIIBPAIBAAJBAN55NcYKZeInyTuhcCwFMhDHCmwaIUSdtXdcbItRB/yfXGBhiex0
+0IaLXQnSU+QZPRZWYqeTEbFSgihqi1PUDy8CAwEAAQJBAQdUx66rfh8sYsgfdcvV
+NoafYpnEcB5s4m/vSVe6SU7dCK6eYec9f9wpT353ljhDUHq3EbmE4foNzJngh35d
+AekCIQDhRQG5Li0Wj8TM4obOnnXUXf1jRv0UkzE9AHWLG5q3AwIhAPzSjpYUDjVW
+MCUXgckTpKCuGwbJk7424Nb8bLzf3kllAiA5mUBgjfr/WtFSJdWcPQ4Zt9KTMNKD
+EUO0ukpTwEIl6wIhAMbGqZK3zAAFdq8DD2jPx+UJXnh0rnOkZBzDtJ6/iN69AiEA
+1Aq8MJgTaYsDQWyU/hDq5YkDJc9e9DSCvUIzqxQWMQE=
+-----END RSA PRIVATE KEY-----`)
