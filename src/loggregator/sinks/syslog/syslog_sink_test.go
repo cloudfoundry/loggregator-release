2e411abb273b2ebb41a8422cb67e3620662e89ae
John Tuley <jtuley@pivotallabs.com>
2014-05-01 14:39:54 -0600

    fix regexp in tests
    Signed-off-by: Johannes Petzold <jpetzold@pivotallabs.com>

--- src/loggregator/sinks/syslog/syslog_sink_test.go
+++ src/loggregator/sinks/syslog/syslog_sink_test.go
@@ -220,7 +220,7 @@ var _ = Describe("SyslogSink", func() {
 				inputChan <- logMessage
 				errorLog := <-errorChannel
 				errorMsg := string(errorLog.GetLogMessage().GetMessage())
-				Expect(errorMsg).To(MatchRegexp(`Syslog Sink syslog://using-fake: Error when dialing out. Backing off for (\d\.\d+ms|\d+us). Err: Error connecting.`))
+				Expect(errorMsg).To(MatchRegexp(`Syslog Sink syslog://using-fake: Error when dialing out. Backing off for (\d(\.\d+)?ms|\d+us). Err: Error connecting.`))
 				Expect(errorLog.GetLogMessage().GetSourceName()).To(Equal("LGR"))
 
 				close(done)
