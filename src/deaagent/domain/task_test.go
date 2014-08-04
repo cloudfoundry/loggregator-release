c43db1ee9a7946ab4dc7d016cb4dd0f23bdeded9
Joseph Rodriguez <jrodriguez@pivotallabs.com>
2014-08-04 14:17:14 -0600

    Convert deaagent/domain tests to Ginkgo
    Signed-off-by: John Tuley <jtuley@pivotallabs.com>

--- src/deaagent/domain/task_test.go
+++ src/deaagent/domain/task_test.go
@@ -2,15 +2,17 @@ package domain_test
 
 import (
 	"deaagent/domain"
-	"github.com/stretchr/testify/assert"
-	"testing"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
-func TestIdentifier(t *testing.T) {
-	task := domain.Task{
-		ApplicationId:       "4aa9506e-277f-41ab-b764-a35c0b96fa1b",
-		WardenJobId:         272,
-		WardenContainerPath: "/var/vcap/data/warden/depot/16vbs06ibo1"}
+var _ = Describe("Task", func() {
+	It("sets identifier correctly", func() {
+		task := domain.Task{
+			ApplicationId:       "4aa9506e-277f-41ab-b764-a35c0b96fa1b",
+			WardenJobId:         272,
+			WardenContainerPath: "/var/vcap/data/warden/depot/16vbs06ibo1"}
 
-	assert.Equal(t, "/var/vcap/data/warden/depot/16vbs06ibo1/jobs/272", task.Identifier())
-}
+		Expect(task.Identifier()).To(Equal("/var/vcap/data/warden/depot/16vbs06ibo1/jobs/272"))
+	})
+})
--- src/deaagent/domain/tasks_reader_test.go
+++ src/deaagent/domain/tasks_reader_test.go
@@ -2,166 +2,154 @@ package domain_test
 
 import (
 	"deaagent/domain"
-	"github.com/stretchr/testify/assert"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 	"io/ioutil"
 	"path"
 	"runtime"
-	"testing"
 )
 
-func TestReadingTask(t *testing.T) {
-	_, filename, _, _ := runtime.Caller(0)
-	filepath := path.Join(path.Dir(filename), "..", "..", "..", "samples", "dea_instances.json")
-	json, _ := ioutil.ReadFile(filepath)
+var _ = Describe("ReadTasks", func() {
+	It("reads tasks", func() {
+		_, filename, _, _ := runtime.Caller(0)
+		filepath := path.Join(path.Dir(filename), "..", "..", "..", "samples", "dea_instances.json")
+		json, _ := ioutil.ReadFile(filepath)
 
-	tasks, err := domain.ReadTasks(json)
+		tasks, err := domain.ReadTasks(json)
 
-	assert.NoError(t, err)
+		Expect(err).NotTo(HaveOccurred())
 
-	assert.Equal(t, 2, len(tasks))
+		Expect(tasks).To(HaveLen(2))
 
-	expectedInstanceTask := domain.Task{
-		ApplicationId:       "4aa9506e-277f-41ab-b764-a35c0b96fa1b",
-		Index:               0,
-		WardenJobId:         272,
-		WardenContainerPath: "/var/vcap/data/warden/depot/16vbs06ibo1",
-		SourceName:          "App",
-		DrainUrls:           []string{}}
+		expectedInstanceTask := domain.Task{
+			ApplicationId:       "4aa9506e-277f-41ab-b764-a35c0b96fa1b",
+			Index:               0,
+			WardenJobId:         272,
+			WardenContainerPath: "/var/vcap/data/warden/depot/16vbs06ibo1",
+			SourceName:          "App",
+			DrainUrls:           []string{}}
 
-	expectedStagingTask := domain.Task{
-		ApplicationId:       "23489sd0-f985-fjga-nsd1-sdg5lhd9nskh",
-		WardenJobId:         355,
-		WardenContainerPath: "/var/vcap/data/warden/depot/16vbs06ibo2",
-		SourceName:          "STG",
-		DrainUrls:           []string{}}
+		expectedStagingTask := domain.Task{
+			ApplicationId:       "23489sd0-f985-fjga-nsd1-sdg5lhd9nskh",
+			WardenJobId:         355,
+			WardenContainerPath: "/var/vcap/data/warden/depot/16vbs06ibo2",
+			SourceName:          "STG",
+			DrainUrls:           []string{}}
 
-	assert.Equal(t, expectedInstanceTask, tasks["/var/vcap/data/warden/depot/16vbs06ibo1/jobs/272"])
-	assert.Equal(t, expectedStagingTask, tasks["/var/vcap/data/warden/depot/16vbs06ibo2/jobs/355"])
-}
+		Expect(tasks["/var/vcap/data/warden/depot/16vbs06ibo1/jobs/272"]).To(Equal(expectedInstanceTask))
+		Expect(tasks["/var/vcap/data/warden/depot/16vbs06ibo2/jobs/355"]).To(Equal(expectedStagingTask))
+	})
 
-func TestReadingMultipleTasks(t *testing.T) {
-	_, filename, _, _ := runtime.Caller(0)
-	filepath := path.Join(path.Dir(filename), "..", "..", "..", "samples", "multi_instances.json")
-	json, _ := ioutil.ReadFile(filepath)
+	It("reads multiple tasks", func() {
+		_, filename, _, _ := runtime.Caller(0)
+		filepath := path.Join(path.Dir(filename), "..", "..", "..", "samples", "multi_instances.json")
+		json, _ := ioutil.ReadFile(filepath)
 
-	tasks, err := domain.ReadTasks(json)
+		tasks, err := domain.ReadTasks(json)
 
-	assert.NoError(t, err)
+		Expect(err).NotTo(HaveOccurred())
 
-	assert.Equal(t, 6, len(tasks))
+		Expect(tasks).To(HaveLen(6))
 
-	var applicationIds [6]string
-	expectedApplicationIds := [6]string{
-		"e0e12b41-78d4-43ff-a5ae-20422bedf22f",
-		"d8df836e-e27d-45d4-a890-b2ce899788a4",
-		"a59ebe7a-002a-4530-8d69-8bf53bc845d5",
-		"cc01f618-6b62-428e-96c6-bc743dd235cd",
-		"09fbc153-e2ed-43b3-8f42-b270d1937ec6",
-		"243a34f-0724-4d9c-a0de-a0811cbabce6",
-	}
+		var applicationIds [6]string
+		expectedApplicationIds := [6]string{
+			"e0e12b41-78d4-43ff-a5ae-20422bedf22f",
+			"d8df836e-e27d-45d4-a890-b2ce899788a4",
+			"a59ebe7a-002a-4530-8d69-8bf53bc845d5",
+			"cc01f618-6b62-428e-96c6-bc743dd235cd",
+			"09fbc153-e2ed-43b3-8f42-b270d1937ec6",
+			"243a34f-0724-4d9c-a0de-a0811cbabce6",
+		}
 
-	i := 0
-	for _, task := range tasks {
-		applicationIds[i] = task.ApplicationId
-		i++
-	}
+		i := 0
+		for _, task := range tasks {
+			applicationIds[i] = task.ApplicationId
+			i++
+		}
 
-	assert.Equal(t, applicationIds, expectedApplicationIds)
-}
+		Expect(applicationIds).To(Equal(expectedApplicationIds))
+	})
 
-func TestReadingMultipleTasksWithDrainUrls(t *testing.T) {
-	_, filename, _, _ := runtime.Caller(0)
-	filepath := path.Join(path.Dir(filename), "..", "..", "..", "samples", "multi_instances.json")
-	json, _ := ioutil.ReadFile(filepath)
+	It("reads multiple tasks with drain urls", func() {
+		_, filename, _, _ := runtime.Caller(0)
+		filepath := path.Join(path.Dir(filename), "..", "..", "..", "samples", "multi_instances.json")
+		json, _ := ioutil.ReadFile(filepath)
 
-	tasks, err := domain.ReadTasks(json)
+		tasks, err := domain.ReadTasks(json)
 
-	assert.NoError(t, err)
+		Expect(err).NotTo(HaveOccurred())
 
-	assert.Nil(t, tasks["/var/vcap/data/warden/depot/170os7ali6q/jobs/15"].DrainUrls)
-	assert.Equal(t, tasks["/var/vcap/data/warden/depot/123ajkljfa/jobs/13"].DrainUrls, []string{})
-	assert.Equal(t, tasks["/var/vcap/data/warden/depot/345asndhaena/jobs/12"].DrainUrls, []string{"syslog://10.20.30.40:8050"})
+		Expect(tasks["/var/vcap/data/warden/depot/170os7ali6q/jobs/15"].DrainUrls).To(BeNil())
+		Expect(tasks["/var/vcap/data/warden/depot/123ajkljfa/jobs/13"].DrainUrls).To(BeEmpty())
+		Expect(tasks["/var/vcap/data/warden/depot/345asndhaena/jobs/12"].DrainUrls).To(ConsistOf("syslog://10.20.30.40:8050"))
 
-	assert.Nil(t, tasks["/var/vcap/data/warden/depot/17fsdo7qpeq/jobs/46"].DrainUrls)
-	assert.Equal(t, tasks["/var/vcap/data/warden/depot/17fsdo7qper/jobs/49"].DrainUrls, []string{})
-	assert.Equal(t, tasks["/var/vcap/data/warden/depot/17fsdo7qpes/jobs/56"].DrainUrls, []string{"syslog://10.20.30.40:8050"})
-}
+		Expect(tasks["/var/vcap/data/warden/depot/17fsdo7qpeq/jobs/46"].DrainUrls).To(BeNil())
+		Expect(tasks["/var/vcap/data/warden/depot/17fsdo7qper/jobs/49"].DrainUrls).To(BeEmpty())
+		Expect(tasks["/var/vcap/data/warden/depot/17fsdo7qpes/jobs/56"].DrainUrls).To(ConsistOf("syslog://10.20.30.40:8050"))
+	})
 
-func TestReadingStartingTasks(t *testing.T) {
-	_, filename, _, _ := runtime.Caller(0)
-	filepath := path.Join(path.Dir(filename), "..", "..", "..", "samples", "starting_instances.json")
-	json, _ := ioutil.ReadFile(filepath)
+	It("reads starting tasks", func() {
+		_, filename, _, _ := runtime.Caller(0)
+		filepath := path.Join(path.Dir(filename), "..", "..", "..", "samples", "starting_instances.json")
+		json, _ := ioutil.ReadFile(filepath)
 
-	tasks, err := domain.ReadTasks(json)
-	assert.NoError(t, err)
+		tasks, err := domain.ReadTasks(json)
+		Expect(err).NotTo(HaveOccurred())
 
-	assert.Equal(t, 1, len(tasks))
+		Expect(tasks).To(HaveLen(1))
 
-	if _, ok := tasks["/var/vcap/data/warden/depot/345asndhaena/jobs/12"]; !ok {
-		t.Errorf("Did not find starting application.")
-	}
-}
+		Expect(tasks).To(HaveKey("/var/vcap/data/warden/depot/345asndhaena/jobs/12"))
+	})
 
-func TestErrorHandlingWhenParsingEmptyData(t *testing.T) {
-	_, err := domain.ReadTasks(make([]byte, 0))
-	assert.Error(t, err)
+	It("handles parsing empty data", func() {
+		_, err := domain.ReadTasks(make([]byte, 0))
+		Expect(err).To(HaveOccurred())
 
-	_, err = domain.ReadTasks(make([]byte, 10))
-	assert.Error(t, err)
+		_, err = domain.ReadTasks(make([]byte, 10))
+		Expect(err).To(HaveOccurred())
 
-	_, err = domain.ReadTasks(nil)
-	assert.Error(t, err)
-}
+		_, err = domain.ReadTasks(nil)
+		Expect(err).To(HaveOccurred())
+	})
 
-func TestErrorHandlingWithBadJson(t *testing.T) {
-	_, err := domain.ReadTasks([]byte(`{ "instances" : [}`))
-	assert.Error(t, err)
-}
+	It("handles bad json", func() {
+		_, err := domain.ReadTasks([]byte(`{ "instances" : [}`))
+		Expect(err).To(HaveOccurred())
+	})
 
-func TestReadingTasksIgnoresNonRunningInstances(t *testing.T) {
-	_, filename, _, _ := runtime.Caller(0)
-	filepath := path.Join(path.Dir(filename), "..", "..", "..", "samples", "instances.crashed.json")
-	json, _ := ioutil.ReadFile(filepath)
+	It("ignores non running instances", func() {
+		_, filename, _, _ := runtime.Caller(0)
+		filepath := path.Join(path.Dir(filename), "..", "..", "..", "samples", "instances.crashed.json")
+		json, _ := ioutil.ReadFile(filepath)
 
-	tasks, err := domain.ReadTasks(json)
+		tasks, err := domain.ReadTasks(json)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(tasks).To(HaveLen(1))
 
-	assert.NoError(t, err)
+		Expect(tasks).To(HaveKey("/var/vcap/data/warden/depot/170os7ali6q/jobs/15"))
+		Expect(tasks).NotTo(HaveKey("/var/vcap/data/warden/depot/345asndhaena/jobs/12"))
+	})
 
-	assert.Equal(t, 1, len(tasks))
+	It("ignores tasks without warden container path or job id", func() {
+		_, filename, _, _ := runtime.Caller(0)
+		filepath := path.Join(path.Dir(filename), "..", "..", "..", "samples", "instances.no_warden_path_or_id.json")
+		json, _ := ioutil.ReadFile(filepath)
 
-	if _, ok := tasks["/var/vcap/data/warden/depot/170os7ali6q/jobs/15"]; !ok {
-		t.Errorf("Did not find active applicaiton.")
-	}
+		tasks, err := domain.ReadTasks(json)
 
-	if _, ok := tasks["/var/vcap/data/warden/depot/345asndhaena/jobs/12"]; ok {
-		t.Errorf("Found crashed applicaiton.")
-	}
-}
+		Expect(err).NotTo(HaveOccurred())
+		Expect(tasks).To(BeEmpty())
+	})
 
-func TestReadingTasksIgnoresTasksWithouteWardenContainerPathOrJobId(t *testing.T) {
-	_, filename, _, _ := runtime.Caller(0)
-	filepath := path.Join(path.Dir(filename), "..", "..", "..", "samples", "instances.no_warden_path_or_id.json")
-	json, _ := ioutil.ReadFile(filepath)
+	It("ignores staging tasks without warden job id", func() {
+		_, filename, _, _ := runtime.Caller(0)
+		filepath := path.Join(path.Dir(filename), "..", "..", "..", "samples", "staging_tasks.no_job_id.json")
+		json, _ := ioutil.ReadFile(filepath)
 
-	tasks, err := domain.ReadTasks(json)
+		tasks, err := domain.ReadTasks(json)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(tasks).To(HaveLen(1))
 
-	assert.NoError(t, err)
-
-	assert.Equal(t, 0, len(tasks))
-}
-
-func TestReadingTasksIgnoreStagingTasksWithoutWardenJobId(t *testing.T) {
-	_, filename, _, _ := runtime.Caller(0)
-	filepath := path.Join(path.Dir(filename), "..", "..", "..", "samples", "staging_tasks.no_job_id.json")
-	json, _ := ioutil.ReadFile(filepath)
-
-	tasks, err := domain.ReadTasks(json)
-
-	assert.NoError(t, err)
-
-	assert.Equal(t, 1, len(tasks))
-
-	if _, ok := tasks["/var/vcap/data/warden/depot/17fsdo7qper/jobs/49"]; !ok {
-		t.Errorf("Did not find staging task with warden job id.")
-	}
-}
+		Expect(tasks).To(HaveKey("/var/vcap/data/warden/depot/17fsdo7qper/jobs/49"), "Did not find staging task with warden job id.")
+	})
+})
