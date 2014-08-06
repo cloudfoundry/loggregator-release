572c0ee32f6a930a7bff44cbd0979193b1055f77
Johannes Petzold <jpetzold@pivotallabs.com>
2014-08-06 16:52:36 -0600

    add MessageAggregator for metron
    [#75143014]

---
+++ src/metron/message_aggregator/message_aggregator.go
@@ -0,0 +1,123 @@
+package message_aggregator
+
+import (
+	"code.google.com/p/gogoprotobuf/proto"
+	"github.com/cloudfoundry/dropsonde/events"
+	"github.com/cloudfoundry/dropsonde/factories"
+	"github.com/cloudfoundry/gosteno"
+	"github.com/davecgh/go-spew/spew"
+	"time"
+)
+
+var MaxTTL = time.Minute
+
+type MessageAggregator interface {
+	Run(inputChan <-chan []byte, outputChan chan<- []byte)
+}
+
+func NewMessageAggregator(logger *gosteno.Logger) MessageAggregator {
+	return &messageAggregator{
+		logger:               logger,
+		startEventsByEventId: make(map[eventId]startEventEntry),
+	}
+}
+
+type messageAggregator struct {
+	logger               *gosteno.Logger
+	startEventsByEventId map[eventId]startEventEntry
+}
+
+type eventId struct {
+	requestId string
+	peerType  events.PeerType
+}
+
+type startEventEntry struct {
+	startEvent *events.HttpStart
+	entryTime  time.Time
+}
+
+func (m *messageAggregator) Run(inputChan <-chan []byte, outputChan chan<- []byte) {
+	for inputMessage := range inputChan {
+		// TODO: don't call for every message if throughput becomes a problem
+		m.cleanupOrphanedHttpStart()
+
+		var envelope events.Envelope
+		err := proto.Unmarshal(inputMessage, &envelope)
+		if err != nil {
+			m.logger.Warnf("unmarshal error %v for message %v", err, inputMessage)
+			outputChan <- inputMessage
+			continue
+		}
+
+		switch envelope.GetEventType() {
+		case events.Envelope_HttpStart:
+			m.handleHttpStart(&envelope)
+		case events.Envelope_HttpStop:
+			startStopMessage := m.handleHttpStop(&envelope)
+			if startStopMessage != nil {
+				outputMessage, _ := proto.Marshal(startStopMessage)
+				outputChan <- outputMessage
+			}
+		default:
+			m.logger.Debugf("passing through message %v", spew.Sprintf("%v", envelope))
+			outputChan <- inputMessage
+		}
+	}
+}
+
+func (m *messageAggregator) handleHttpStart(envelope *events.Envelope) {
+	m.logger.Debugf("handling HTTP start message %v", spew.Sprintf("%v", envelope))
+	startEvent := envelope.GetHttpStart()
+
+	requestId := factories.StringFromUUID(startEvent.RequestId)
+	eventId := eventId{requestId: requestId, peerType: startEvent.GetPeerType()}
+	m.startEventsByEventId[eventId] = startEventEntry{startEvent: startEvent, entryTime: time.Now()}
+}
+
+func (m *messageAggregator) handleHttpStop(envelope *events.Envelope) *events.Envelope {
+	m.logger.Debugf("handling HTTP stop message %v", spew.Sprintf("%v", envelope))
+	stopEvent := envelope.GetHttpStop()
+
+	requestId := factories.StringFromUUID(stopEvent.RequestId)
+	eventId := eventId{requestId: requestId, peerType: stopEvent.GetPeerType()}
+
+	startEventEntry, ok := m.startEventsByEventId[eventId]
+	if !ok {
+		m.logger.Warnf("no matching HTTP start message found for %v", eventId)
+		return nil
+	}
+
+	delete(m.startEventsByEventId, eventId)
+	startEvent := startEventEntry.startEvent
+
+	return &events.Envelope{
+		Origin:    envelope.Origin,
+		EventType: events.Envelope_HttpStartStop.Enum(),
+		HttpStartStop: &events.HttpStartStop{
+			StartTimestamp:  startEvent.Timestamp,
+			StopTimestamp:   stopEvent.Timestamp,
+			RequestId:       startEvent.RequestId,
+			PeerType:        startEvent.PeerType,
+			Method:          startEvent.Method,
+			Uri:             startEvent.Uri,
+			RemoteAddress:   startEvent.RemoteAddress,
+			UserAgent:       startEvent.UserAgent,
+			StatusCode:      stopEvent.StatusCode,
+			ContentLength:   stopEvent.ContentLength,
+			ParentRequestId: startEvent.ParentRequestId,
+			ApplicationId:   startEvent.ApplicationId,
+			InstanceIndex:   startEvent.InstanceIndex,
+			InstanceId:      startEvent.InstanceId,
+		},
+	}
+}
+
+func (m *messageAggregator) cleanupOrphanedHttpStart() {
+	currentTime := time.Now()
+	for key, eventEntry := range m.startEventsByEventId {
+		if currentTime.Sub(eventEntry.entryTime) > MaxTTL {
+			delete(m.startEventsByEventId, key)
+		}
+	}
+}
---
+++ src/metron/message_aggregator/message_aggregator_test.go
@@ -0,0 +1,193 @@
+package message_aggregator_test
+
+import (
+	"code.google.com/p/gogoprotobuf/proto"
+	"github.com/cloudfoundry/dropsonde/events"
+	"github.com/cloudfoundry/dropsonde/factories"
+	"github.com/cloudfoundry/loggregatorlib/loggertesthelper"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"metron/message_aggregator"
+	"time"
+)
+
+var _ = Describe("MessageAggregator", func() {
+	var (
+		inputChan         chan []byte
+		outputChan        chan []byte
+		runComplete       chan struct{}
+		messageAggregator message_aggregator.MessageAggregator
+	)
+
+	BeforeEach(func() {
+		inputChan = make(chan []byte, 10)
+		outputChan = make(chan []byte)
+		runComplete = make(chan struct{})
+		messageAggregator = message_aggregator.NewMessageAggregator(loggertesthelper.Logger())
+
+		go func() {
+			messageAggregator.Run(inputChan, outputChan)
+			close(runComplete)
+		}()
+	})
+
+	AfterEach(func() {
+		close(inputChan)
+		Eventually(runComplete).Should(BeClosed())
+	})
+
+	It("passes non-marshallable messages through", func() {
+		inputMessage := []byte{1, 2, 3}
+		inputChan <- inputMessage
+
+		outputMessage := <-outputChan
+		Expect(outputMessage).To(Equal(inputMessage))
+	})
+
+	It("passes heartbeats through", func() {
+		inputMessage := createHeartbeatMessage()
+		inputChan <- inputMessage
+
+		outputMessage := <-outputChan
+		Expect(outputMessage).To(Equal(inputMessage))
+	})
+
+	Context("single StartStop message", func() {
+		var outputMessage events.Envelope
+		BeforeEach(func() {
+			inputChan <- createStartMessage(123, events.PeerType_Client)
+			inputChan <- createStopMessage(123, events.PeerType_Client)
+
+			outputMessageBytes := <-outputChan
+			proto.Unmarshal(outputMessageBytes, &outputMessage)
+		})
+
+		It("aggregates HTTP start+stop messages", func() {
+			Expect(outputMessage.GetEventType()).To(Equal(events.Envelope_HttpStartStop))
+		})
+
+		It("populates all fields in the StartStop message correctly", func() {
+			Expect(&outputMessage).To(Equal(createStartStopMessage(123, events.PeerType_Client)))
+		})
+	})
+
+	It("does not send a combined event if there only is a stop event", func() {
+		inputChan <- createStopMessage(123, events.PeerType_Client)
+		Consistently(outputChan).ShouldNot(Receive())
+	})
+
+	Context("message expiry", func() {
+		var originalTTL time.Duration
+		BeforeEach(func() {
+			originalTTL = message_aggregator.MaxTTL
+			message_aggregator.MaxTTL = 0
+		})
+		AfterEach(func() {
+			message_aggregator.MaxTTL = originalTTL
+		})
+
+		It("does not send a combined event if the stop event doesn't arrive within the TTL", func() {
+			inputChan <- createStartMessage(123, events.PeerType_Client)
+			time.Sleep(1)
+			inputChan <- createStopMessage(123, events.PeerType_Client)
+
+			Consistently(outputChan).ShouldNot(Receive())
+		})
+	})
+})
+
+func createStartMessage(requestId uint64, peerType events.PeerType) []byte {
+	message, _ := proto.Marshal(&events.Envelope{
+		Origin:    proto.String("fake-origin-1"),
+		EventType: events.Envelope_HttpStart.Enum(),
+		HttpStart: &events.HttpStart{
+			Timestamp: proto.Int64(1),
+			RequestId: &events.UUID{
+				Low:  proto.Uint64(requestId),
+				High: proto.Uint64(requestId + 1),
+			},
+			PeerType:      &peerType,
+			Method:        events.Method_GET.Enum(),
+			Uri:           proto.String("fake-uri-1"),
+			RemoteAddress: proto.String("fake-remote-addr-1"),
+			UserAgent:     proto.String("fake-user-agent-1"),
+			ParentRequestId: &events.UUID{
+				Low:  proto.Uint64(2),
+				High: proto.Uint64(3),
+			},
+			ApplicationId: &events.UUID{
+				Low:  proto.Uint64(4),
+				High: proto.Uint64(5),
+			},
+			InstanceIndex: proto.Int32(6),
+			InstanceId:    proto.String("fake-instance-id-1"),
+		},
+	})
+
+	return message
+}
+
+func createStopMessage(requestId uint64, peerType events.PeerType) []byte {
+	message, _ := proto.Marshal(&events.Envelope{
+		Origin:    proto.String("fake-origin-2"),
+		EventType: events.Envelope_HttpStop.Enum(),
+		HttpStop: &events.HttpStop{
+			Timestamp: proto.Int64(100),
+			Uri:       proto.String("fake-uri-2"),
+			RequestId: &events.UUID{
+				Low:  proto.Uint64(requestId),
+				High: proto.Uint64(requestId + 1),
+			},
+			PeerType:      &peerType,
+			StatusCode:    proto.Int32(103),
+			ContentLength: proto.Int64(104),
+			ApplicationId: &events.UUID{
+				Low:  proto.Uint64(105),
+				High: proto.Uint64(106),
+			},
+		},
+	})
+
+	return message
+}
+
+func createStartStopMessage(requestId uint64, peerType events.PeerType) *events.Envelope {
+	return &events.Envelope{
+		Origin:    proto.String("fake-origin-2"),
+		EventType: events.Envelope_HttpStartStop.Enum(),
+		HttpStartStop: &events.HttpStartStop{
+			StartTimestamp: proto.Int64(1),
+			StopTimestamp:  proto.Int64(100),
+			RequestId: &events.UUID{
+				Low:  proto.Uint64(requestId),
+				High: proto.Uint64(requestId + 1),
+			},
+			PeerType:      &peerType,
+			Method:        events.Method_GET.Enum(),
+			Uri:           proto.String("fake-uri-1"),
+			RemoteAddress: proto.String("fake-remote-addr-1"),
+			UserAgent:     proto.String("fake-user-agent-1"),
+			StatusCode:    proto.Int32(103),
+			ContentLength: proto.Int64(104),
+			ParentRequestId: &events.UUID{
+				Low:  proto.Uint64(2),
+				High: proto.Uint64(3),
+			},
+			ApplicationId: &events.UUID{
+				Low:  proto.Uint64(4),
+				High: proto.Uint64(5),
+			},
+			InstanceIndex: proto.Int32(6),
+			InstanceId:    proto.String("fake-instance-id-1"),
+		},
+	}
+}
+
+func createHeartbeatMessage() []byte {
+	message, _ := proto.Marshal(&events.Envelope{
+		Origin:    proto.String("fake-origin-3"),
+		EventType: events.Envelope_Heartbeat.Enum(),
+		Heartbeat: factories.NewHeartbeat(1, 2, 3),
+	})
+	return message
+}
