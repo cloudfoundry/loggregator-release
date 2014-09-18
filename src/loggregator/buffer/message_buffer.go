02d86258708954805c9ad4ec1ef49f0e180b102f
Joseph Rodriguez <jrodriguez@pivotal.io>
2014-09-18 14:57:12 -0600

    delete The Loggregator
    [#76284328]
    
    Signed-off-by: Johannes Petzold <jpetzold@pivotal.io>

--- src/loggregator/buffer/message_buffer.go
+++
@@ -1,9 +0,0 @@
-package buffer
-
-import "github.com/cloudfoundry/loggregatorlib/logmessage"
-
-type MessageBuffer interface {
-	GetOutputChannel() <-chan *logmessage.Message
-	CloseOutputChannel()
-	Run()
-}
