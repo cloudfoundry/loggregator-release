4f2aaae532960a035bfe66f6f2f1ce7e275da08e
Alex Jackson <ajackson@pivotal.io>
2015-03-02 13:31:01 -0700

    fix paths for concorse, assume gomega and ginkgo are in the docker image

--- bin/concourse_build.sh
+++ bin/concourse_build.sh
@@ -1,5 +1,27 @@
+#!/bin/bash
+
+set -x 
 set -e
 
-go get -v github.com/onsi/gomega
+env
 
-`dirname $0`/test
+mkdir ${PWD}/gotemp
+
+go version
+
+export GOPATH=${PWD}/gotemp
+
+go get golang.org/x/tools/cmd/vet
+go get golang.org/x/tools/cmd/cover
+
+export GOROOT=/usr/src/go
+export PATH=${GOPATH}/bin:${GOROOT}/bin:/bin:/usr/bin:/usr/local/bin
+export TERM=xterm
+
+env
+
+go install github.com/onsi/ginkgo/ginkgo
+
+cd $PWD/loggregator
+
+bin/test
