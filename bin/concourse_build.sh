c63b19620147731a3e6f78ca6173e81a0bbb003a
Mike Gehard <mgehard@pivotal.io>
2015-03-30 15:20:55 -0600

    Get unit test running on concourse
    This included getting rid of a dependency on a syslog
    drain that was burried in another dependency.
    
    Signed-off-by: Maya Rosecrance <mrosecrance@pivotal.io>

--- bin/concourse_build.sh
+++ bin/concourse_build.sh
@@ -3,25 +3,11 @@
 set -x 
 set -e
 
-env
+cd loggregator
 
-mkdir ${PWD}/gotemp
-
-go version
-
-export GOPATH=${PWD}/gotemp
-
-go get golang.org/x/tools/cmd/vet
-go get golang.org/x/tools/cmd/cover
-
-export GOROOT=/usr/src/go
-export PATH=${GOPATH}/bin:${GOROOT}/bin:/bin:/usr/bin:/usr/local/bin
+export GOPATH=`pwd`
 export TERM=xterm
 
-env
-
 go install github.com/onsi/ginkgo/ginkgo
-
-cd $PWD/loggregator
 
 bin/test
