dcf7c4fadec90858d5da4173dd92d4944a3b124f
Maya Rosecrance <mrosecrance@pivotal.io>
2015-03-31 10:56:01 -0600

    Simple golang REPL to issue statsd commands to a local statsd server
    [#91450916]
    
    Signed-off-by: Rohit Kumar <rokumar@pivotal.io>

---
+++ src/tools/statsdGoClient/README.md
@@ -0,0 +1,17 @@
+#Summary
+
+This is a simple go client which can be used to send metrics to a statsd server running locally on port 8125.
+The client reads the standard input or any file passed in as an argument for statsd commands. The
+valid commands are:
+
+timing <name> <value>
+gauge <name> <value>
+count <name> <value> [sample_rate]
+
+# Running
+
+Run:
+
+```
+go run main.go
+```
---
+++ src/tools/statsdGoClient/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+    "github.com/cactus/go-statsd-client/statsd"
+    "bufio"
+    "os"
+    "strings"
+    "fmt"
+    "strconv"
+)
+
+func main() {
+    client, err := statsd.NewClient("127.0.0.1:8125", "")
+    if err != nil {
+        fmt.Printf("Error connecting to statsd server: %v\n", err.Error())
+        return
+    }
+    defer client.Close()
+
+    scanner := bufio.NewScanner(os.Stdin)
+    for scanner.Scan() {
+        line := scanner.Text()
+        inputs := strings.Split(line, " ")
+        if (len(inputs) < 3) {
+            fmt.Printf("Wrong number of inputs, 3 needed at least\n")
+            continue
+        }
+        statsdType := inputs[0]
+        name := inputs[1]
+        value, _ := strconv.ParseInt(inputs[2], 10, 0)
+        switch statsdType {
+            case "count":
+            var sampleRate float32 = 1.0
+            if (len(inputs) == 4) {
+                rate, _ := strconv.ParseFloat(inputs[3], 32)
+                sampleRate = float32(rate)
+            }
+            client.Inc(name, value, sampleRate)
+            case "gauge":
+            client.Gauge(name, value, 1.0)
+            case "timing":
+            client.Timing(name, value, 1.0)
+        }
+    }
+}
