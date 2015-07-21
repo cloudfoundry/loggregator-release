df297a6c96aaedff3b0dc37f8ec6273e1dd41236
David Sabeti <dsabeti@pivotal.io>
2015-07-21 16:56:35 -0600

    Add metron legacy benchmark
    [#99315896]
    
    Signed-off-by: Dwayne Schultz <dschultz@pivotal.io>

--- src/integration_tests/metron/benchmark/message_loss_benchmark_test.go
+++ src/integration_tests/metron/benchmark/message_loss_benchmark_test.go
@@ -39,4 +39,33 @@ var _ = Describe("MessageLossBenchmark", func() {
 		Expect(err).ToNot(HaveOccurred())
 		b.RecordValue("message throughput", messageThroughput)
 	}, 3)
+
+	Measure("message loss and throughput with legacy and dropsonde messages", func(b Benchmarker) {
+		pathToMetronBenchmarkExec, err := gexec.Build("tools/legacymetronbenchmark")
+		Expect(err).NotTo(HaveOccurred())
+
+		command := exec.Command(pathToMetronBenchmarkExec, "-writeRate", "5000", "-interval",
+			"10s", "-stopAfter", "11s")
+		outBuffer := bytes.NewBuffer(nil)
+		errBuffer := bytes.NewBuffer(nil)
+		benchmarkSession, err := gexec.Start(command, outBuffer, errBuffer)
+		Expect(err).ToNot(HaveOccurred())
+		Eventually(benchmarkSession, 15).Should(gexec.Exit())
+		out := outBuffer.String()
+		Expect(out).To(ContainSubstring("PercentLoss"))
+		lines := strings.Split(out, "\n")
+		Expect(lines).To(HaveLen(4))
+		values := strings.Split(lines[1], ", ")
+		Expect(values).To(HaveLen(3))
+
+		value := strings.Split(values[2], "%")[0]
+		percentLoss, err := strconv.ParseFloat(value, 64)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(percentLoss).To(BeNumerically("<", 1.0))
+		b.RecordValue("message loss (percent)", percentLoss)
+
+		messageThroughput, err := strconv.ParseFloat(values[0], 64)
+		Expect(err).ToNot(HaveOccurred())
+		b.RecordValue("message throughput", messageThroughput)
+	}, 3)
 })
