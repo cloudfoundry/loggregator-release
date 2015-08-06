b0ac51efff5fe62aa5ca7c49990b43818e9bed73
Ivan Sim <ihcsim@gmail.com>
2015-08-06 14:01:50 -0600

    Remove log streaming test and its dependencies
    After a lot of conversation, these tests may be suited most for CATs (where they actually already live). For now, we will proceed to add coverage for metrics in LATs, and then revisit the possiblity of putting these tests in LATS as well / instead.
    
    Signed-off-by: David Sabeti <dsabeti@pivotal.io>

--- src/lats/lats_suite_test.go
+++ src/lats/lats_suite_test.go
@@ -4,29 +4,10 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
-	"github.com/cloudfoundry-incubator/cf-test-helpers/helpers"
 	"testing"
 )
 
-var config helpers.Config
-
 func TestLats(t *testing.T) {
 	RegisterFailHandler(Fail)
-
-	var environment *helpers.Environment
-
-	BeforeSuite(func() {
-		config = helpers.LoadConfig()
-
-		context := helpers.NewContext(config)
-		environment = helpers.NewEnvironment(context)
-
-		environment.Setup()
-	})
-
-	AfterSuite(func() {
-		environment.Teardown()
-	})
-
 	RunSpecs(t, "Lats Suite")
 }
