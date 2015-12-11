5e5dc04101222aa2a4d7d3a6794e30b112ea5ff4
Warren Fernandes <wfernandes@pivotal.io>
2015-12-11 15:49:05 -0700

    Adds appveyor build files
    [#108282142]
    
    Signed-off-by: Rohit Kumar <rokumar@pivotal.io>

---
+++ bin/test.bat
@@ -0,0 +1,13 @@
+@echo on
+
+rmdir /S /Q output
+mkdir output
+SET PATH=%GOROOT%;%PATH%;%DEVENV_PATH%
+
+SET GOBIN=%CD%\bin
+SET PATH=%GOBIN%;%PATH%
+:: Install metron, it contains all relevant gocode inside itself.
+SET GOPATH=%CD%
+go install github.com/onsi/ginkgo/ginkgo || exit /b 1
+ginkgo -r -noColor src\metron || exit /b 1
+go install metron || exit /b 1
