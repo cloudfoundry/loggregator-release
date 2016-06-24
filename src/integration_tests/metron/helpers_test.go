ca08f63863b3c66679dc0b5790b249826ffc6d01
Jason Keene <jkeene@pivotal.io>
2016-06-24 10:39:43 -0600

    Remove old helpers and etcd test that is no longer needed
    [#121903317]
    
    Signed-off-by: Wesley Jeanette <wjeanette@pivotal.io>

--- src/integration_tests/metron/helpers_test.go
+++ src/integration_tests/metron/helpers_test.go
@@ -1,8 +1,6 @@
 package metron_test
 
 import (
-	"crypto/hmac"
-	"crypto/sha256"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -18,11 +16,6 @@ import (
 	dopplerConfig "doppler/config"
 	metronConfig "metron/config"
 
-	"github.com/cloudfoundry/sonde-go/events"
-	"github.com/gogo/protobuf/proto"
-
-	"github.com/cloudfoundry/dropsonde/factories"
-	"github.com/nu7hatch/gouuid"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gbytes"
@@ -35,6 +28,10 @@ const (
 	jobName          = "test-job-name"
 	jobIndex         = "42"
 )
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
 
 func setupEtcd() (func(), string) {
 	By("starting etcd")
@@ -207,210 +204,4 @@ func setupMetron(etcdClientURL, proto string) (func(), int) {
 		os.Remove(metronCfgFile.Name())
 		metronSession.Kill()
 	}, metronPort
-}
-
-func basicValueMetric(name string, value float64, unit string) *events.ValueMetric {
-	return &events.ValueMetric{
-		Name:  proto.String(name),
-		Value: proto.Float64(value),
-		Unit:  proto.String(unit),
-	}
-}
-
-func addDefaultTags(envelope *events.Envelope) *events.Envelope {
-	envelope.Deployment = proto.String("deployment-name")
-	envelope.Job = proto.String("test-component")
-	envelope.Index = proto.String("42")
-	envelope.Ip = proto.String(localIPAddress)
-
-	return envelope
-}
-
-func basicValueMessage() []byte {
-	message, _ := proto.Marshal(basicValueMessageEnvelope())
-	return message
-}
-
-func basicValueMessageEnvelope() *events.Envelope {
-	return &events.Envelope{
-		Origin:    proto.String("fake-origin-2"),
-		EventType: events.Envelope_ValueMetric.Enum(),
-		ValueMetric: &events.ValueMetric{
-			Name:  proto.String("fake-metric-name"),
-			Value: proto.Float64(42),
-			Unit:  proto.String("fake-unit"),
-		},
-	}
-}
-
-func basicCounterEventMessage() []byte {
-	message, _ := proto.Marshal(&events.Envelope{
-		Origin:    proto.String("fake-origin-2"),
-		EventType: events.Envelope_CounterEvent.Enum(),
-		CounterEvent: &events.CounterEvent{
-			Name:  proto.String("fake-counter-event-name"),
-			Delta: proto.Uint64(1),
-		},
-	})
-
-	return message
-}
-
-func basicHTTPStartMessage(requestId string) []byte {
-	message, _ := proto.Marshal(basicHTTPStartMessageEnvelope(requestId))
-	return message
-}
-
-func basicHTTPStartMessageEnvelope(requestId string) *events.Envelope {
-	uuid, _ := uuid.ParseHex(requestId)
-	return &events.Envelope{
-		Origin:    proto.String("fake-origin-2"),
-		EventType: events.Envelope_HttpStart.Enum(),
-		HttpStart: &events.HttpStart{
-			Timestamp:     proto.Int64(12),
-			RequestId:     factories.NewUUID(uuid),
-			PeerType:      events.PeerType_Client.Enum(),
-			Method:        events.Method_GET.Enum(),
-			Uri:           proto.String("some uri"),
-			RemoteAddress: proto.String("some address"),
-			UserAgent:     proto.String("some user agent"),
-		},
-	}
-}
-
-func basicHTTPStopMessage(requestId string) []byte {
-	message, _ := proto.Marshal(basicHTTPStopMessageEnvelope(requestId))
-	return message
-}
-
-func basicHTTPStopMessageEnvelope(requestId string) *events.Envelope {
-	uuid, _ := uuid.ParseHex(requestId)
-	return &events.Envelope{
-		Origin:    proto.String("fake-origin-2"),
-		EventType: events.Envelope_HttpStop.Enum(),
-		HttpStop: &events.HttpStop{
-			Timestamp:     proto.Int64(12),
-			Uri:           proto.String("some uri"),
-			RequestId:     factories.NewUUID(uuid),
-			PeerType:      events.PeerType_Client.Enum(),
-			StatusCode:    proto.Int32(404),
-			ContentLength: proto.Int64(98475189),
-		},
-	}
-}
-
-func eventsLogMessage(appID int, message string, timestamp time.Time) *events.Envelope {
-	return &events.Envelope{
-		Origin:    proto.String("legacy"),
-		EventType: events.Envelope_LogMessage.Enum(),
-		LogMessage: &events.LogMessage{
-			Message:        []byte(message),
-			MessageType:    events.LogMessage_OUT.Enum(),
-			Timestamp:      proto.Int64(timestamp.UnixNano()),
-			AppId:          proto.String(string(appID)),
-			SourceType:     proto.String("fake-source-id"),
-			SourceInstance: proto.String("fake-source-id"),
-		},
-	}
-}
-
-func eventuallyListensForUDP(address string) net.PacketConn {
-	var testServer net.PacketConn
-
-	Eventually(func() error {
-		var err error
-		testServer, err = net.ListenPacket("udp4", address)
-		return err
-	}).Should(Succeed())
-
-	return testServer
-}
-
-func eventuallyListensForTLS(address string) net.Listener {
-	var listener net.Listener
-
-	Eventually(func() error {
-		var err error
-		listener, err = net.Listen("tcp", address)
-		return err
-	}).Should(Succeed())
-
-	return listener
-}
-
-type MetronInput struct {
-	metronConn   net.Conn
-	stopTheWorld chan struct{}
-}
-
-func (input *MetronInput) WriteToMetron(unsignedMessage []byte) {
-	ticker := time.NewTicker(10 * time.Millisecond)
-
-	for {
-		select {
-		case <-input.stopTheWorld:
-			ticker.Stop()
-			return
-		case <-ticker.C:
-			input.metronConn.Write(unsignedMessage)
-		}
-	}
-}
-
-type FakeDoppler struct {
-	packetConn   net.PacketConn
-	stopTheWorld chan struct{}
-}
-
-func (d *FakeDoppler) ReadIncomingMessages(signature []byte) chan signedMessage {
-	messageChan := make(chan signedMessage, 1000)
-
-	go func() {
-		readBuffer := make([]byte, 65535)
-		for {
-			select {
-			case <-d.stopTheWorld:
-				return
-			default:
-				readCount, _, _ := d.packetConn.ReadFrom(readBuffer)
-				readData := make([]byte, readCount)
-				copy(readData, readBuffer[:readCount])
-
-				// Only signed messages get placed on messageChan
-				if gotSignedMessage(readData, signature) {
-					msg := signedMessage{
-						signature: readData[:len(signature)],
-						message:   readData[len(signature):],
-					}
-					messageChan <- msg
-				}
-			}
-		}
-	}()
-
-	return messageChan
-}
-
-func (d *FakeDoppler) Close() {
-	d.packetConn.Close()
-}
-
-type signedMessage struct {
-	signature []byte
-	message   []byte
-}
-
-func sign(message []byte) signedMessage {
-	expectedEnvelope := addDefaultTags(basicValueMessageEnvelope())
-	expectedMessage, _ := proto.Marshal(expectedEnvelope)
-
-	mac := hmac.New(sha256.New, []byte("shared_secret"))
-	mac.Write(expectedMessage)
-
-	signature := mac.Sum(nil)
-	return signedMessage{signature: signature, message: expectedMessage}
-}
-
-func gotSignedMessage(readData, signature []byte) bool {
-	return len(readData) > len(signature)
 }
--- src/integration_tests/metron/metron_suite_test.go
+++ src/integration_tests/metron/metron_suite_test.go
@@ -1,116 +1,41 @@
 package metron_test
 
 import (
-	"fmt"
-	"integration_tests/runners"
-	"io/ioutil"
-	"math/rand"
-	"net/http"
 	"os"
 	"testing"
-	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
-	"github.com/cloudfoundry/storeadapter"
-	"github.com/cloudfoundry/storeadapter/storerunner/etcdstorerunner"
-	"github.com/onsi/ginkgo/config"
 	"github.com/onsi/gomega/gexec"
-	"github.com/pivotal-golang/localip"
 )
 
-func TestIntegrationTest(t *testing.T) {
+func TestMetron(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "Metron Integration Suite")
+	RunSpecs(t, "Metron Integration Test Suite")
 }
 
 var (
-	tmpdir         string
-	port           int
-	metronRunner   *runners.MetronRunner
-	etcdRunner     *etcdstorerunner.ETCDClusterRunner
-	etcdAdapter    storeadapter.StoreAdapter
-	localIPAddress string
-
 	etcdPath    string
 	dopplerPath string
 	metronPath  string
 )
 
-var _ = SynchronizedBeforeSuite(func() []byte {
-	// TODO: rip this out
-	metronPathOld, err := gexec.Build("metron", "-race")
-	Expect(err).ShouldNot(HaveOccurred())
-	return []byte(metronPathOld)
-}, func(path []byte) {
-	metronPathOld := string(path)
-
+var _ = BeforeSuite(func() {
 	var err error
-	tmpdir, err = ioutil.TempDir("", "metronintg")
-	Expect(err).NotTo(HaveOccurred())
 
-	etcdPort := 5800 + (config.GinkgoConfig.ParallelNode)*10
-	etcdRunner = etcdstorerunner.NewETCDClusterRunner(etcdPort, 1, nil)
-	etcdRunner.Start()
-	etcdAdapter = etcdRunner.Adapter(nil)
+	metronPath, err = gexec.Build("metron", "-race")
+	Expect(err).ToNot(HaveOccurred())
 
-	localIPAddress, err = localip.LocalIP()
-	Expect(err).NotTo(HaveOccurred())
+	dopplerPath, err = gexec.Build("doppler", "-race")
+	Expect(err).ToNot(HaveOccurred())
 
-	waitOnURL(fmt.Sprintf("http://127.0.0.1:%d", etcdPort))
-	port = 51000 + config.GinkgoConfig.ParallelNode*10
-	metronRunner = &runners.MetronRunner{
-		Path:          metronPathOld,
-		TempDir:       tmpdir,
-		LegacyPort:    port,
-		MetronPort:    port + 1,
-		DropsondePort: 3457 + config.GinkgoConfig.ParallelNode*10,
-		EtcdRunner:    etcdRunner,
-
-		CertFile: "../fixtures/client.crt",
-		KeyFile:  "../fixtures/client.key",
-		CAFile:   "../fixtures/loggregator-ca.crt",
-	}
-
-	{
-		var err error
-
-		rand.Seed(time.Now().UnixNano())
-
-		metronPath, err = gexec.Build("metron", "-race")
-		Expect(err).ToNot(HaveOccurred())
-
-		dopplerPath, err = gexec.Build("doppler", "-race")
-		Expect(err).ToNot(HaveOccurred())
-
-		etcdPath, err = gexec.Build("github.com/coreos/etcd", "-race")
-		Expect(err).ToNot(HaveOccurred())
-	}
+	etcdPath, err = gexec.Build("github.com/coreos/etcd", "-race")
+	Expect(err).ToNot(HaveOccurred())
 })
 
-var _ = SynchronizedAfterSuite(func() {
-	if etcdRunner != nil {
-		etcdRunner.Stop()
-	}
-	os.RemoveAll(tmpdir)
-}, func() {
-	gexec.CleanupBuildArtifacts()
-
-	{
-		os.Remove(etcdPath)
-		os.Remove(dopplerPath)
-		os.Remove(metronPath)
-	}
+var _ = AfterSuite(func() {
+	os.Remove(etcdPath)
+	os.Remove(dopplerPath)
+	os.Remove(metronPath)
 })
-
-var _ = BeforeEach(func() {
-	etcdRunner.Reset()
-})
-
-func waitOnURL(url string) {
-	Eventually(func() error {
-		_, err := http.Get(url)
-		return err
-	}, 3).ShouldNot(HaveOccurred())
-}
