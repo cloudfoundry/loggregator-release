44a6f034ec9675f883649c66b75baad7cd869399
Jason Keene <jkeene@pivotal.io>
2016-06-29 14:31:36 -0600

    Add max count to logemitter
    [#122660231]
    
    Signed-off-by: Warren Fernandes <wfernandes@pivotal.io>

--- src/tools/logemitter/main.go
+++ src/tools/logemitter/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/nu7hatch/gouuid"
@@ -21,24 +22,29 @@ func init() {
 }
 
 func main() {
-	go func() {
-		http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-			rw.WriteHeader(200)
-		})
-		err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-		if err != nil {
-			panic(err)
-		}
-	}()
-
 	uuid, err := uuid.NewV4()
 	if err != nil {
 		log.Fatal(err)
 	}
 	guid := uuid.String()
 
-	for i := 0; ; i++ {
-		fmt.Printf("logemitter guid: %s msg: %d\n", guid, i)
-		time.Sleep(delay)
+	max, err := strconv.Atoi(os.Getenv("MAX"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		for i := 0; i < max; i++ {
+			fmt.Printf("logemitter guid: %s msg: %d\n", guid, i)
+			time.Sleep(delay)
+		}
+	}()
+
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		rw.WriteHeader(200)
+	})
+	err = http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
