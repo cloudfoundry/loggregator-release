7297a5ea7d003d40d5393b750b753d9d9d727174
Nino Khodabandeh <ninok@hpe.com>
2016-07-28 15:47:31 -0600

    Generates configuration file for boshhmforwarder integration tests to create a random info port
    [#126997187]
    
    Signed-off-by: Jeremy Alvis <jalvis@pivotal.io>

--- src/integration_tests/boshhmforwarder/integration_tests_suite_test.go
+++ src/integration_tests/boshhmforwarder/integration_tests_suite_test.go
@@ -1,18 +1,22 @@
 package integration_tests_test
 
 import (
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
-
+	"encoding/json"
+	"io/ioutil"
+	"net"
+	"os"
 	"os/exec"
 	"testing"
 
+	"boshhmforwarder/config"
+	"boshhmforwarder/logging"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gexec"
 )
 
-var (
-	session *gexec.Session
-)
+var session *gexec.Session
 
 func TestIntegrationTests(t *testing.T) {
 	RegisterFailHandler(Fail)
@@ -23,6 +27,7 @@ var _ = BeforeSuite(func() {
 	boshForwarderExecutable, err := gexec.Build("boshhmforwarder", "-race")
 	Expect(err).ToNot(HaveOccurred())
 
+	createConfig()
 	command := exec.Command(boshForwarderExecutable, "--configPath", "./integration_config.json")
 
 	session, err = gexec.Start(command, gexec.NewPrefixedWriter("[o][boshhmforwarder]", GinkgoWriter), gexec.NewPrefixedWriter("[e][boshhmforwarder]", GinkgoWriter))
@@ -36,4 +41,30 @@ var _ = AfterSuite(func() {
 	}
 
 	gexec.CleanupBuildArtifacts()
+	err := os.Remove("integration_config.json")
+	Expect(err).ToNot(HaveOccurred())
 })
+
+func createConfig() {
+	config := &config.Config{
+		IncomingPort: 4002,
+		InfoPort:     testPort(),
+		DebugPort:    -1,
+		MetronPort:   10001,
+		LogLevel:     logging.DEBUG,
+	}
+
+	j, err := json.Marshal(config)
+	Expect(err).ToNot(HaveOccurred())
+
+	err = ioutil.WriteFile("integration_config.json", j, 0644)
+	Expect(err).ToNot(HaveOccurred())
+}
+
+func testPort() int {
+	add, _ := net.ResolveTCPAddr("tcp", ":0")
+	l, _ := net.ListenTCP("tcp", add)
+	defer l.Close()
+	port := l.Addr().(*net.TCPAddr).Port
+	return port
+}
