23720b43ef4f7a0111dd180d25b3e2319fc15dbf
Andrew Poydence <apoydence@pivotal.io>
2016-12-07 14:14:02 -0700

    LATs uses a colocated metron [#133603381]
    Signed-off-by: JT Archie <jtarchie@gmail.com>

--- src/lats/lats_suite_test.go
+++ src/lats/lats_suite_test.go
@@ -1,7 +1,6 @@
 package lats_test
 
 import (
-	"os/exec"
 	"testing"
 	"time"
 
@@ -9,7 +8,6 @@ import (
 	"github.com/gogo/protobuf/proto"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"github.com/onsi/gomega/gexec"
 
 	latsConfig "lats/config"
 	"lats/helpers"
@@ -20,35 +18,19 @@ var config *latsConfig.TestConfig
 func TestLats(t *testing.T) {
 	RegisterFailHandler(Fail)
 
-	var metronSession *gexec.Session
 	config = latsConfig.Load()
 
 	BeforeSuite(func() {
 		config.SaveMetronConfig()
 		helpers.Initialize(config)
-		metronSession = setupMetron()
-	})
-
-	AfterSuite(func() {
-		metronSession.Kill().Wait()
+		setupMetron()
 	})
 
 	RunSpecs(t, "Lats Suite")
 }
 
-func setupMetron() *gexec.Session {
-	pathToMetronExecutable, err := gexec.Build("metron")
-	Expect(err).ShouldNot(HaveOccurred())
-
-	command := exec.Command(pathToMetronExecutable, "--config=fixtures/metron.json", "--debug")
-	metronSession, err := gexec.Start(command, gexec.NewPrefixedWriter("[o][metron]", GinkgoWriter), gexec.NewPrefixedWriter("[e][metron]", GinkgoWriter))
-	Expect(err).ShouldNot(HaveOccurred())
-
-	// TODO: We should have some better way of ensuring we're ready
+func setupMetron() {
 	time.Sleep(10 * time.Second)
-	Consistently(metronSession.Exited).ShouldNot(BeClosed())
-
-	return metronSession
 }
 
 func createLogMessage(appID string) *events.Envelope {
