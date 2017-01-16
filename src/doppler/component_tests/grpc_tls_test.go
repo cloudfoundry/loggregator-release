168b6cef597d00ed5031d376e92a997252e7c745
Jason Keene <jkeene@pivotal.io>
2017-01-16 12:01:05 -0700

    Clean up gRPC component test
    [#136965797]
    
    Signed-off-by: Eno Compton <ecompton@pivotal.io>

--- src/doppler/component_tests/grpc_tls_test.go
+++ src/doppler/component_tests/grpc_tls_test.go
@@ -19,75 +19,17 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-func setupDopplerEnv() (string, func()) {
-	etcdCleanup, etcdURI := testservers.StartTestEtcd()
-
-	// TODO: get random port from kernel....
-	udpAddr, err := net.ResolveUDPAddr("udp", ":12345")
-	Expect(err).ToNot(HaveOccurred())
-
-	_, err = net.ListenUDP("udp", udpAddr)
-	dopplerCleanup, _, dopplerPort := testservers.StartDoppler(
-		testservers.BuildDopplerConfig(etcdURI, 12345),
-	)
-	uri := fmt.Sprintf("localhost:%d", dopplerPort)
-	return uri, func() {
-		etcdCleanup()
-		dopplerCleanup()
-	}
-}
-
-func setupIngestor(uri string) plumbing.DopplerIngestor_PusherClient {
-	tlsConfig, err := plumbing.NewMutualTLSConfig(
-		testservers.ClientCertFilePath(),
-		testservers.ClientKeyFilePath(),
-		testservers.CAFilePath(),
-		"doppler",
-	)
-	Expect(err).ToNot(HaveOccurred())
-	transportCreds := credentials.NewTLS(tlsConfig)
-	c, err := grpc.Dial(uri, grpc.WithTransportCredentials(transportCreds))
-	Expect(err).ToNot(HaveOccurred())
-	client := plumbing.NewDopplerIngestorClient(c)
-
-	pusher, err := client.Pusher(context.Background())
-	Expect(err).ToNot(HaveOccurred())
-
-	return pusher
-}
-
-func setupSubscriber(uri string) plumbing.Doppler_SubscribeClient {
-	tlsConfig, err := plumbing.NewMutualTLSConfig(
-		testservers.ClientCertFilePath(),
-		testservers.ClientKeyFilePath(),
-		testservers.CAFilePath(),
-		"doppler",
-	)
-	Expect(err).ToNot(HaveOccurred())
-	transportCreds := credentials.NewTLS(tlsConfig)
-	c, err := grpc.Dial(uri, grpc.WithTransportCredentials(transportCreds))
-	Expect(err).ToNot(HaveOccurred())
-	client := plumbing.NewDopplerClient(c)
-
-	subscriber, err := client.Subscribe(context.Background(), &plumbing.SubscriptionRequest{
-		ShardID: "test-shard",
-	})
-	Expect(err).ToNot(HaveOccurred())
-
-	return subscriber
-}
-
 var _ = Describe("gRPC TLS", func() {
-	It("works", func() {
-		uri, cleanup := setupDopplerEnv()
+	It("supports v1 api", func() {
+		hostPort, cleanup := setupDopplerEnv()
 		defer cleanup()
-		subscriber := setupSubscriber(uri)
-		pusher := setupIngestor(uri)
+		subscriber := setupSubscriber(hostPort)
+		sender := setupV1Ingestor(hostPort)
 
-		_, data := buildContainerMetric()
+		_, data := buildV1ContainerMetric()
 		message := &plumbing.EnvelopeData{data}
 		Consistently(func() error {
-			return pusher.Send(message)
+			return sender.Send(message)
 		}, 5).Should(Succeed())
 
 		f := func() []byte {
@@ -99,7 +41,68 @@ var _ = Describe("gRPC TLS", func() {
 	})
 })
 
-func buildContainerMetric() (*events.Envelope, []byte) {
+func setupDopplerEnv() (string, func()) {
+	etcdCleanup, etcdURI := testservers.StartTestEtcd()
+
+	By("listen for doppler writes into metron")
+	// TODO: get random port from kernel....
+	udpAddr, err := net.ResolveUDPAddr("udp", ":12345")
+	Expect(err).ToNot(HaveOccurred())
+	_, err = net.ListenUDP("udp", udpAddr)
+
+	dopplerCleanup, _, dopplerPort := testservers.StartDoppler(
+		testservers.BuildDopplerConfig(etcdURI, 12345),
+	)
+	hostPort := fmt.Sprintf("localhost:%d", dopplerPort)
+	return hostPort, func() {
+		etcdCleanup()
+		dopplerCleanup()
+	}
+}
+
+func setupV1Ingestor(hostPort string) plumbing.DopplerIngestor_PusherClient {
+	tlsConfig, err := plumbing.NewMutualTLSConfig(
+		testservers.ClientCertFilePath(),
+		testservers.ClientKeyFilePath(),
+		testservers.CAFilePath(),
+		"doppler",
+	)
+	Expect(err).ToNot(HaveOccurred())
+	transportCreds := credentials.NewTLS(tlsConfig)
+	c, err := grpc.Dial(hostPort, grpc.WithTransportCredentials(transportCreds))
+	Expect(err).ToNot(HaveOccurred())
+	client := plumbing.NewDopplerIngestorClient(c)
+
+	ctx, _ := context.WithDeadline(context.Background(), time.Now().Add(3*time.Second))
+	pusher, err := client.Pusher(ctx)
+	Expect(err).ToNot(HaveOccurred())
+
+	return pusher
+}
+
+func setupSubscriber(hostPort string) plumbing.Doppler_SubscribeClient {
+	tlsConfig, err := plumbing.NewMutualTLSConfig(
+		testservers.ClientCertFilePath(),
+		testservers.ClientKeyFilePath(),
+		testservers.CAFilePath(),
+		"doppler",
+	)
+	Expect(err).ToNot(HaveOccurred())
+	transportCreds := credentials.NewTLS(tlsConfig)
+	c, err := grpc.Dial(hostPort, grpc.WithTransportCredentials(transportCreds))
+	Expect(err).ToNot(HaveOccurred())
+	client := plumbing.NewDopplerClient(c)
+
+	ctx, _ := context.WithDeadline(context.Background(), time.Now().Add(10*time.Second))
+	subscriber, err := client.Subscribe(ctx, &plumbing.SubscriptionRequest{
+		ShardID: "test-shard",
+	})
+	Expect(err).ToNot(HaveOccurred())
+
+	return subscriber
+}
+
+func buildV1ContainerMetric() (*events.Envelope, []byte) {
 	envelope := &events.Envelope{
 		Origin:    proto.String("doppler"),
 		EventType: events.Envelope_ContainerMetric.Enum(),
