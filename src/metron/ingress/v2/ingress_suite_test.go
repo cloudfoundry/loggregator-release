895b5229d3347927757d46ef5ebeadd60857a76c
JT Archie <jarchie@pivotal.io>
2017-03-01 16:29:30 -0700

    Capture test log output
    Signed-off-by: Andrew Poydence <apoydence@pivotal.io>

--- src/metron/ingress/v2/ingress_suite_test.go
+++ src/metron/ingress/v2/ingress_suite_test.go
@@ -1,6 +1,8 @@
 package v2_test
 
 import (
+	"log"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
@@ -8,6 +10,7 @@ import (
 )
 
 func TestIngress(t *testing.T) {
+	log.SetOutput(GinkgoWriter)
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Ingress V2 Suite")
 }
