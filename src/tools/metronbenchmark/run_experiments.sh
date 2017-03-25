a9883ce28ff2561199adab37402b916905297043
Eno Compton <eno4@ecom.com>
2017-03-25 11:06:54 -0600

    Organize Metron using app-internal structure
    - Remove duplicate LICENSE and NOTICE which are available at the top
    level of loggregator
    - Remove tools/metronbenchmark which wrote into metron using the UDP API
    and is unused.

--- src/tools/metronbenchmark/run_experiments.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir -p results
-for INTERVAL in 1s 1m; do
-  for WRITE_RATE in 15000 30000 50000; do
-    go run main.go -interval=$INTERVAL -writeRate=$WRITE_RATE | tee results/old_metron_${WRITE_RATE}_${INTERVAL}.csv
-  done
-done
