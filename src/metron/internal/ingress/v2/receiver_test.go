4985563a9aed2707472eaf9de26b5e6a383c90f2
Brady Love <blove@pivotal.io>
2017-05-04 12:30:14 -0600

    remove helheim from metron ingress v2 tests
    [#142098227]
    
    Signed-off-by: Warren Fernandes <warren.f.fernandes@gmail.com>

--- src/metron/internal/ingress/v2/receiver_test.go
+++ src/metron/internal/ingress/v2/receiver_test.go
@@ -15,40 +15,81 @@ var _ = Describe("Receiver", func() {
 	var (
 		rx *ingress.Receiver
 
-		mockDataSetter *mockDataSetter
-		mockSender     *mockSender
+		spySender *SpySender
+		spySetter *SpySetter
 	)
 
 	BeforeEach(func() {
-		mockSender = newMockSender()
-		mockDataSetter = newMockDataSetter()
+		spySender = NewSpySender()
+		spySetter = NewSpySetter()
 
-		rx = ingress.NewReceiver(mockDataSetter)
+		rx = ingress.NewReceiver(spySetter)
 	})
 
 	It("calls set on the data setter with the data", func() {
 		e := &v2.Envelope{
 			SourceId: "some-id",
 		}
-		mockSender.RecvOutput.Ret0 <- e
-		mockSender.RecvOutput.Ret1 <- nil
-		mockSender.RecvOutput.Ret0 <- e
-		mockSender.RecvOutput.Ret1 <- nil
-		mockSender.RecvOutput.Ret0 <- nil
-		mockSender.RecvOutput.Ret1 <- io.EOF
 
-		rx.Sender(mockSender)
+		spySender.recvResponses <- RecvResponse{
+			envelope: e,
+		}
+		spySender.recvResponses <- RecvResponse{
+			envelope: e,
+		}
+		spySender.recvResponses <- RecvResponse{
+			err: io.EOF,
+		}
 
-		Eventually(mockDataSetter.SetInput.E).Should(Receive(Equal(e)))
-		Eventually(mockDataSetter.SetInput.E).Should(Receive(Equal(e)))
+		rx.Sender(spySender)
+
+		Eventually(spySetter.envelopes).Should(Receive(Equal(e)))
+		Eventually(spySetter.envelopes).Should(Receive(Equal(e)))
 	})
 
 	It("returns an error when receive fails", func() {
-		close(mockSender.RecvOutput.Ret0)
-		mockSender.RecvOutput.Ret1 <- errors.New("error occurred")
+		spySender.recvResponses <- RecvResponse{
+			err: errors.New("error occurred"),
+		}
 
-		err := rx.Sender(mockSender)
+		err := rx.Sender(spySender)
 
 		Expect(err).To(HaveOccurred())
 	})
 })
+
+type RecvResponse struct {
+	envelope *v2.Envelope
+	err      error
+}
+
+type SpySender struct {
+	v2.Ingress_SenderServer
+	recvResponses chan RecvResponse
+}
+
+func NewSpySender() *SpySender {
+	return &SpySender{
+		recvResponses: make(chan RecvResponse, 100),
+	}
+}
+
+func (s *SpySender) Recv() (*v2.Envelope, error) {
+	resp := <-s.recvResponses
+
+	return resp.envelope, resp.err
+}
+
+type SpySetter struct {
+	envelopes chan *v2.Envelope
+}
+
+func NewSpySetter() *SpySetter {
+	return &SpySetter{
+		envelopes: make(chan *v2.Envelope, 100),
+	}
+}
+
+func (s *SpySetter) Set(e *v2.Envelope) {
+	s.envelopes <- e
+}
