4c49c603492b533fcf39e1333c70790e180dc464
Andrew Poydence <apoydence@pivotal.io>
2017-05-19 11:39:34 -0600

    add start command to latency push script
    Signed-off-by: Brady Love <blove@pivotal.io>

--- src/tools/latency/cf/push.sh
+++ src/tools/latency/cf/push.sh
@@ -7,7 +7,7 @@ function target_url {
         python -c 'import json, sys; print json.load(sys.stdin).get("doppler_logging_endpoint")'
 }
 app_name=${1:-loggregator-latency}
-cf push "$app_name" -b binary_buildpack --no-start -m 64M
+cf push "$app_name" -b binary_buildpack --no-start -m 64M -c ./latency
 cf set-env "$app_name" TARGET_URL "$(target_url)"
 cf set-env "$app_name" TOKEN "$(cf oauth-token)"
 cf start "$app_name"
