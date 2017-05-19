1864723077c8ef92d5754bd93754572e0e3a39ae
Brady Love <blove@pivotal.io>
2017-05-19 11:17:24 -0600

    decrease latency test memory size
    Signed-off-by: Andrew Poydence <apoydence@pivotal.io>

--- src/tools/latency/cf/push.sh
+++ src/tools/latency/cf/push.sh
@@ -7,7 +7,7 @@ function target_url {
         python -c 'import json, sys; print json.load(sys.stdin).get("doppler_logging_endpoint")'
 }
 app_name=${1:-loggregator-latency}
-cf push "$app_name" -b binary_buildpack --no-start
+cf push "$app_name" -b binary_buildpack --no-start -m 64M
 cf set-env "$app_name" TARGET_URL "$(target_url)"
 cf set-env "$app_name" TOKEN "$(cf oauth-token)"
 cf start "$app_name"
