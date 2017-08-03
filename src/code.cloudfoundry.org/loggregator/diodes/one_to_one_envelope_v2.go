7e26ad8e283b68a3a5a43904c5c8e3bea9ab0c83
Brady Love <love.brady@gmail.com>
2017-08-03 07:48:39 -0600

    Add go doc to diodes

--- src/code.cloudfoundry.org/loggregator/diodes/one_to_one_envelope_v2.go
+++ src/code.cloudfoundry.org/loggregator/diodes/one_to_one_envelope_v2.go
@@ -11,16 +11,22 @@ type OneToOneEnvelopeV2 struct {
 	d *gendiodes.Poller
 }
 
+// NewOneToOneEnvelopeV2 initializes a new one to one diode for V2 envelopes
+// of a given size and alerter. The alerter is called whenever data is dropped
+// with an integer representing the number of V2 envelopes that were dropped.
 func NewOneToOneEnvelopeV2(size int, alerter gendiodes.Alerter, opts ...gendiodes.PollerConfigOption) *OneToOneEnvelopeV2 {
 	return &OneToOneEnvelopeV2{
 		d: gendiodes.NewPoller(gendiodes.NewOneToOne(size, alerter), opts...),
 	}
 }
 
+// Set inserts the given V2 envelope into the diode.
 func (d *OneToOneEnvelopeV2) Set(data *v2.Envelope) {
 	d.d.Set(gendiodes.GenericDataType(data))
 }
 
+// TryNext returns the next V2 envelope to be read from the diode. If the
+// diode is empty it will return a nil envelope and false for the bool.
 func (d *OneToOneEnvelopeV2) TryNext() (*v2.Envelope, bool) {
 	data, ok := d.d.TryNext()
 	if !ok {
@@ -30,6 +36,9 @@ func (d *OneToOneEnvelopeV2) TryNext() (*v2.Envelope, bool) {
 	return (*v2.Envelope)(data), true
 }
 
+// Next will return the next V2 envelope to be read from the diode. If the
+// diode is empty this method will block until anenvelope is available to be
+// read.
 func (d *OneToOneEnvelopeV2) Next() *v2.Envelope {
 	data := d.d.Next()
 	return (*v2.Envelope)(data)
