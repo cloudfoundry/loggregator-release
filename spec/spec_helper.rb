268b285f26bd1ff19d2b41a37f05eec65b658b65
Eno Compton <eno4@ecom.com>
2017-10-26 19:56:58 -0600

    Move test helper function to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,4 +19,19 @@ def render_template(props, job:, template:, spec: InstanceSpec.new, links: [])
   end
 end
 
+def render_monit_config(properties, spec: InstanceSpec.new, links: [])
+  release_path = File.join(File.dirname(__FILE__), '../')
+  release = Bosh::Template::Test::ReleaseDir.new(release_path)
+  job = release.job('metron_agent_windows')
 
+  # Hack our monit template into the bosh templates
+  job.instance_variable_get(:@templates)["monit"] = '../monit'
+  template = job.template('../monit')
+  p = template.instance_variable_get(:@template_path)
+  p.gsub!("templates/monit", "monit")
+  template.instance_variable_set(:@template_path, p)
+
+  rendered = template.render(properties, spec: spec, consumes: links)
+
+  JSON.parse(rendered)["processes"].first["env"]
+end
