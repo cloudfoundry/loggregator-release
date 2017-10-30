8bcbfed5eb320c1a8e3c68d882f056594d17f07e
Andrew Poydence <apoydence@pivotal.io>
2017-10-30 15:57:53 -0600

    Fix error in test assertion
    Signed-off-by: Eno Compton <ecompton@pivotal.io>

--- spec/doppler/templates/environment.sh.erb_spec.rb
+++ spec/doppler/templates/environment.sh.erb_spec.rb
@@ -9,10 +9,8 @@ RSpec.describe "Doppler Environment" do
         "grpc_port" => 1111,
         "health_addr" => "localhost:3333",
         "maxRetainedLogMessages" => 100,
-        "message_drain_buffer_size" => 20,
         "pprof_port" => 2222,
         "sink_inactivity_timeout_seconds" => 10,
-        "unmarshaller_count" => 30,
       },
       "metron_endpoint" => {
         "host" => "10.0.0.10",
@@ -47,10 +45,8 @@ RSpec.describe "Doppler Environment" do
       "ROUTER_CA_FILE" => "/var/vcap/jobs/doppler/config/certs/loggregator_ca.crt",
       "ROUTER_CIPHER_SUITES" => "a,b",
       "ROUTER_MAX_RETAINED_LOG_MESSAGES" => "100",
-      "ROUTER_MESSAGE_DRAIN_BUFFER_SIZE" => "20",
       "ROUTER_CONTAINER_METRIC_TTL_SECONDS" => "60",
       "ROUTER_SINK_INACTIVITY_TIMEOUT_SECONDS" => "10",
-      "ROUTER_UNMARSHALLER_COUNT" => "30",
       "ROUTER_PPROF_PORT" => "2222",
       "ROUTER_HEALTH_ADDR" => "localhost:3333",
     }
