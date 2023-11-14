7e2a0e007a0c1faef330ba7fb15e005e902e274e
Carson Long <lcarson@vmware.com>
2023-11-14 13:57:09 -0800

    chore: Remove exiter.ps1
    This file is unnecessary as there are no Windows jobs in this release.

--- src/exiter.ps1
+++
@@ -1,6 +0,0 @@
-exit 0
-# The reason for needing this file is to make `bosh export-release` happy.
-# BOSH doesn't know what job to export, so it tries to export a windows job
-# against a linux stemcell. This is a file that will cause a bash script to
-# immediately exit (such as when exporting a release) but be ignored when run
-# in powershell.
